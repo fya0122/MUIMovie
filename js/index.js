552422f7d7621ee2d00dd843f89ff5cd023ae04e
fya0122 <18730930122@163.com>
2019-05-01 22:53:25 +0800

    '引入了vue'

--- js/index.js
+++ js/index.js
@@ -15,9 +15,7 @@
 			pageW = window.innerWidth,
 			currIndex = 0;
 
-		/**
-		 * 根据判断view控件点击位置判断切换的tab
-		 */
+		// 根据判断view控件点击位置判断切换的tab
 		nview.addEventListener('click', function(e) {
 			var clientX = e.clientX;
 			if(clientX > 0 && clientX <= parseInt(pageW * 0.33)) {
@@ -45,5 +43,59 @@
 				activePage = targetPage;
 			}
 		});
+
+		// 给搜索框添加点击事件
+		mui('.wrap-search')[0].addEventListener('tap', function() {
+			mui.alert(123)
+		})
+
+		var data_movies = new Vue({
+			el: '#movies',
+			data: {
+				movies: []
+			}
+		})
+
+		// 这个函数是转换数据接口用的
+		function convert(items) {
+			var newItems = []
+			items.forEach(function(item) {
+				// 类型
+				var genres = item.genres.toString().replace(/,/g, '/')
+				// 导演
+				var directors = ''
+				for(var i = 0; i < item.directors.length; i++) {
+					directors += item.directors[i].name
+					if(i !== item.directors.length - 1) {
+						directors += '/'
+					}
+				}
+				// 演员
+				var casts = ''
+				for(var i = 0; i < item.casts.length; i++) {
+					casts += item.casts[i].name
+					if(i !== item.casts.length - 1) {
+						casts += '/'
+					}
+				}
+				newItems.push({
+					id: item.id,
+					title: item.title,
+					genres: genres,
+					cover: item.images.large,
+					score: item.rating.average,
+					directors: directors,
+					casts: casts
+				})
+			})
+			return newItems
+		}
+
+		// 获取首页数据
+		mui.getJSON(baseUrl + '/v2/movie/in_theaters', {}, function(res) {
+			data_movies.movies = convert(res.subjects)
+			var myscroll = new BScroll(document.getElementById('list_wrap'))
+		})
 	});
+
 })();
